72b974213c546e86f60a2a12a23f6df1812a2541
Gilbert Velebil <lukas.crhak@post.cz>
2016-11-20 20:21:18 +0100

    fixed loop logic, added logging incl. logging UNIQUE CONSTRAINT exception #2

---
+++ test_unit/sqlite_utils_ut.py
@@ -0,0 +1,20 @@
+import unittest
+import unittest.mock as mock
+import shared_constants as CONST
+import os
+
+def prnt(x):
+	print('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	print(x)
+	print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
+
+class CreateTable(unittest.TestCase):
+	pass
+
+
+class LogRecord(unittest.TestCase):
+	pass
+
+
+class ReadLatestRecord(unittest.TestCase):
+	pass
